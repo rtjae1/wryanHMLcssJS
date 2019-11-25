159022dbbf1fc5efef8cc5d8924941a76bf4b464
rtjae1 <ryan.t.jae@gmail.com>
2019-11-25 17:24:44 -0500

    first run through, no working javascript, links aren't working exactly as they should

---
+++ js/responsiveNavbar.js
@@ -0,0 +1,12 @@
+selector(s: '.menu').addEventListener('click', function() {
+    this.classList.toggle(token: 'open')
+    selector()
+})
+
+
+
+
+
+function selector(s) {
+    return document.querySelector(s);
+}
